900b46beab0ad3a24d81e063958c970957f7ddc7
jimdmm <jimdouglasmmferreira@gmail.com>
2026-02-02 11:29:21 -0300

    refactor: utilizando o EnvService no main.ts

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
 
-  const port = 3000
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
 
   await app.listen(port)
 }
